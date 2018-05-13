0bd0e7e25214f84f4fa03ddcaaf0cd689591b6eb
Carlos <carluizfla@hotmail.com>
2018-05-13 12:09:45 -0300

    acrescentado api do youtube e criado search_bar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyBDDsQ3hEtE0q6TnZiZRYV0z2JGh5CT_fI';
 
 // Create a new component. this component should produce some HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+
 }
 
 //Take this component's generated HTML and put i on the page (in the DOM)
